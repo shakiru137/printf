97a9c8ed90a42ee105fb1805c10c6778e48dbb9f
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-04-25 12:31:32 -0700

    updated

--- handlers.c
+++ handlers.c
@@ -91,8 +91,8 @@ return (write(1, &orr[1], index - 1) + write(1, &orr[index], length));
 		else if (!(flag & F_MINUS) && d == '0')
 		{
 		if (ch)
-			buffer[--d_start] = ch, return (write(1, &buffer[d_start], i - d_start) +
-		write(1, &buffer[index], length - (1 - d_start)));
+			buffer[--d_start] = ch, return (write(1, &orr[d_start], i - d_start) +
+		write(1, &orr[index], length - (1 - d_start)));
 		}
 	}
 	if (ch)
